5afc4b131072aea09311733276778743c955af33
shawnm7474 <shawnm74@gmail.com>
2020-10-13 21:45:17 -0500

    added db folder, schema and seeds files for db

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+### Pet Safe Tracker seeds
+
+INSERT INTO UserTable (UserName, UserPassword, FirstName, LastName, PhoneNum, Email, Role, ZipCode) 
+	VALUES ('My User Name', 'My Password', 'Joe', 'Boxer', 5555555555, 'test@gmail.com', false, 77777);
