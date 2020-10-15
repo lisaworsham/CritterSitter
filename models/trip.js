2e6ceb60d5977c413815167310f18fdfb8689bb8
Tajah Johnson <tajah93@hotmail.com>
2020-10-15 18:22:06 -0500

    Trip model created

---
+++ models/trip.js
@@ -0,0 +1,47 @@
+module.exports = function(sequelize, DataTypes) {   
+   const trip = sequelize.define('trip', {
+        TripName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        FromDate: {
+            type: DataTypes.DATE(6),
+            allowNull: false,
+            validate: {
+                isDate: true
+            }
+        },
+        ToDate: {
+            type: DataTypes.DATE(6),
+            allowNull: false,
+            validate: {
+                isDate: true
+            }
+        },
+        EmergencyContact: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        Comments: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        }, 
+        timestamps: true,
+        createdAt: true
+    });
+    trip.associate = function(models) {
+        trip.belongsTo(models.userTable, {
+          foreignKey: {
+            allowNull: false
+          }
+        });
+      };
+    trip.associate = function(models) {
+        trip.belongsTo(models.petProfile, {
+          foreignKey: {
+            allowNull: false
+          }
+        });
+      };
+        return trip
+}
