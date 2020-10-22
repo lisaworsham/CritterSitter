fc8c7499585080d42b7c06acbbd1f23d3072ecd6
Tajah Johnson <tajah93@hotmail.com>
2020-10-22 13:12:43 -0500

    Updating seeds/deleted migrations folder for seeders

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,6 @@
-### Pet Safe Tracker seeds
+SELECT * FROM petsafetracker_db.User;
+INSERT INTO petsafetracker_db.User (Email, UserPassword, FirstName, LastName, PhoneNum, ZipCode, PetOwner, PetSitter, createdAt, updatedAt) 
+	VALUES ('meg_griff@toon.com', 'megg', 'Meg', 'Griffin', 1234567890, 77090, false, true, '2020-10-21 02:04:58', '2020-10-21 02:04:58');
 
-INSERT INTO User (Email, UserPassword, FirstName, LastName, PhoneNum, ZipCode, PetOwner, PetSitter) 
-	VALUES ('meg_griff@toon.com', 'megg', 'Meg', 'Griffin', 1234567890, 77090, false, true);
-
-INSERT INTO User (Email, UserPassword, FirstName, LastName, PhoneNum, ZipCode, PetOwner, PetSitter) 
-	VALUES ('dalmationlove@dis.org', 'puppies', 'Cruella', 'de Vil', 1234567890, 77101, false, true);
+INSERT INTO petsafetracker_db.User (Email, UserPassword, FirstName, LastName, PhoneNum, ZipCode, PetOwner, PetSitter, createdAt, updatedAt) 
+	VALUES ('dalmationlove@dis.org', 'puppies', 'Cruella', 'de Vil', 1234567890, 77101, false, true, '2020-10-20 02:04:58', '2020-10-20 02:04:58');
